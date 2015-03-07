c6503b16a7dbfb7ca4806b49bf711e8c88e0f4a1
Danny Shmueli <dannyshmueli@gmail.com>
2015-03-08 00:19:56 +0200

    initial commit

---
+++ Blur-with-IB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  blurtest
+//
+//  Created by Danny Shmueli on 2/28/15.
+//  Copyright (c) 2015 omdan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
